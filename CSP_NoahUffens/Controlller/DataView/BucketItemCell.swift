43b7195b132f618dcdc9ba6dc9f8900ef0bd6dde
Firebeard0 <31518739+Firebeard0@users.noreply.github.com>
2018-01-09 12:13:36 -0700

    working on data view controller

---
+++ CSP_NoahUffens/Controlller/DataView/BucketItemCell.swift
@@ -0,0 +1,24 @@
+//
+//  BucketItemCell.swift
+//  CSP_NoahUffens
+//
+//  Created by Uffens, Noah on 1/9/18.
+//  Copyright © 2018 Uffens, Noah. All rights reserved.
+//
+
+import UIKit
+
+class BucketItemCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ CSP_NoahUffens/Model/BucketItem.swift
@@ -0,0 +1,9 @@
+//
+//  BucketItem.swift
+//  CSP_NoahUffens
+//
+//  Created by Uffens, Noah on 1/9/18.
+//  Copyright © 2018 Uffens, Noah. All rights reserved.
+//
+
+import Foundation
